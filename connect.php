d2414565c1277a3762dad39cbb8320d3107ff9e5
Vincent Brodard <vincent.brodard@gmail.com>
2017-01-24 13:41:37 +0100

    Generation de la liste des personnes dans game-session, avec leur statut

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php 
 try{
-	$bdd = new PDO('mysql:host=localhost;dbname=Zzznake;charset=utf8', 'root', '');	
+	//$bdd = new PDO('mysql:host=localhost;dbname=Zzznake;charset=utf8', 'debian-sys-maint', 'oeyY3OBVaj7qihof');	
 	}catch (Exception $e){
 			die('Error : '.$e->getMessage());
 	}
